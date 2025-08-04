9080d2fccf287e7ef1fda44ef86dc918a3b8d140
mwangiii <mwangi8kevin@gmail.com>
2025-08-04 07:06:40 +0300

    chore:fixing nginx error

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 from app import app
-from prometheus_flask_exporter import PrometheusMetrics
-
-metrics = PrometheusMetrics(app)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
