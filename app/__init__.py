509e7520e9eb12300d8411a6030f6e9418527b74
mwangiii <mwangi8kevin@gmail.com>
2025-08-04 05:54:33 +0300

    chore:making sure all code is pushed

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,25 @@ from config import config
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 import os
+from prometheus_flask_exporter import PrometheusMetrics 
+
+import os
 
 # Initialize Flask application
 app = Flask(__name__)
 
+# ADD Prometheus Exporter
+metrics = PrometheusMetrics(app)
+
+
+
 # Load database configuration from database.ini using config function
 db_params = config()
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:QJKZuKBpgoInspGTLMrgWxlkfrSiLXKu@monorail.proxy.rlwy.net:17210/railway"
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 # connect the postgress to localhost db
-# app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}/{db_params['database']}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '4f8b31dc8ee3437486e3424bcb2d6f0b'
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
