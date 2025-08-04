2baf6496c829e142b65e68187921a4590df41dc7
mwangiii <mwangi8kevin@gmail.com>
2025-08-04 07:00:18 +0300

    code base clean up

--- app/__init__.py
+++ app/__init__.py
@@ -4,25 +4,15 @@ from config import config
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 import os
-from prometheus_flask_exporter import PrometheusMetrics 
 
-import os
-
-# Initialize Flask application
 app = Flask(__name__)
 
-# ADD Prometheus Exporter
-metrics = PrometheusMetrics(app)
-
-
-
-# Load database configuration from database.ini using config function
+# Load DB configuration
 db_params = config()
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
-# connect the postgress to localhost db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '4f8b31dc8ee3437486e3424bcb2d6f0b'
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
@@ -32,17 +22,11 @@ db = SQLAlchemy(app)
 jwt = JWTManager(app)
 migrate = Migrate(app, db)
 
-# Import models (this should be done before calling db.create_all())
+# Import models before creating tables
 from app import models
 
-# Create database tables if they do not exist
 with app.app_context():
     db.create_all()
 
 # Import routes
 from app import routes
-
-# This will allow you to manage database migrations using Flask-Migrate
-if __name__ == '__main__':
-    app.run()
-    
